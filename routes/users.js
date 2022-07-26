4add64afcfa3ae543558328fa9af3b26809e8f99
Anton <94136841+kodareAnton@users.noreply.github.com>
2022-07-26 20:12:46 +0200

    almost finnish

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const { json } = require('express/lib/response');
 var router = express.Router();
+const {ObjectId} = require('mongodb');
 
 
 // Får användare från datorbas
@@ -12,20 +13,29 @@ router.get('/', function(req, res, next) {
     let printUsers = '<div><h2>Våra users</h2>'
 
     for (user in results){
-      printUsers += '<div>' + results[user].username + '</div>'
+      printUsers += '<h1>' + results[user].username + '</h1>' 
     }
     printUsers += "</div>"
     res.send(printUsers);
   })
 });
 
+// Hittar en och skriver ut det
+
+router.post('/user', function(req ,res, next){
+  req.app.locals.db.collection('users').findOne({_id: ObjectId(req.body._id)})
+  .then(results =>{
+    res.json(results)
+  })
+});
 
 // hämtar users till frontend
 router.post('/loggin', function(req, res, next) {
 
-  req.app.locals.db.collection('users').find().toArray()
+  console.log(req.body);
+  req.app.locals.db.collection('users').findOne({username: req.body.username, password: req.body.password})
   .then(results =>{
-    console.log(results);
+    res.json(results)
   })
 });
 
@@ -39,28 +49,20 @@ router.post('/add', function(req,res){
   })
 });
 
-// router.get('/loggin', function (req, res, next){
-//   req.app.locals.db.collection('users').find().toArray()
-//   .then(results =>{
-
-//     let users = results
-    
-//     res.send(users);
-//   })
-// })
-
-// router.post("/loggin", function (req, res, next) {
-
-//   const obj = JSON.parse(JSON.stringify(req.body));
-
-//   if (obj.username == "admin" && obj.password == "admin") {
-//     console.log("OOK");
-    
-//   } else {
-//     console.log("Inte OK");
-//   }
-
-//   return;
-// });
+// Ändra gammal Posts // PUT
+router.put('/:id', function(req,res){
+  console.log(req.params.id)
+  console.log(req.body.subscribe);
+  try {
+    req.app.locals.db.collection('users').findOneAndUpdate({_id: Object(req.params.id)}, {$set: {subscribe: req.body.subscribe}})
+    .then(result =>{
+      res.status(200).send(JSON.stringify('200'));
+      console.log('uppdaterar användare');
+    })
+  } catch (err){
+    console.log(err);
+    res.status(400).send(err)
+  }
+});
 
 module.exports = router;
